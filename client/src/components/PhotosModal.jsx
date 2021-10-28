e4fecc60e5f8b3debe081f1361190662ec96c0b8
Alek <aleksebastian@gmail.com>
2021-10-28 14:16:49 -0500

    updated thumbnail module import and enforced formatting style in index.html

--- client/src/components/PhotosModal.jsx
+++ client/src/components/PhotosModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import Thumbnails from "./Photos.jsx";
+import Thumbnails from "./Thumbnails.jsx";
 
 const PhotosModalWrapper = styled.div`
   position: absolute;
