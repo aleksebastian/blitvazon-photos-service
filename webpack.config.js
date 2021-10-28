9b2871868ce359d2d898ac11d80771817ad37eb6
Alek <aleksebastian@gmail.com>
2021-10-28 14:08:57 -0500

    Added compression dependency and webpack configuration for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,47 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
+
 module.exports = {
   entry: `${__dirname}/client/src/App.jsx`,
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: [/\.jsx$/],
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
   output: {
-    filename: 'photos_bundle.js',
-    path: __dirname + '/public'
-  }
+    filename: "photos_bundle.js",
+    path: __dirname + "/public",
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
+    }),
+  ],
 };
