6a5cbd7a61848bd85da52c0a8b75fd3756af3c91
Alek <aleksebastian@gmail.com>
2021-10-30 23:17:55 -0500

    Updated db import name to use model name

--- database/query.js
+++ database/query.js
@@ -1,30 +1,34 @@
-const mongoose = require('mongoose');
-const db = require('./index.js');
+const Mongoose = require("mongoose");
+const Photo = require("./index.js");
 
 const getProductPrimaryPhoto = async (id) => {
-  let productInfo = await db.Photo.findOne({id: id}).select('primaryUrl');
+  let productInfo = await Photo.findOne({ id: id }).select("primaryUrl");
   let productPrimaryPhotoUrl = productInfo.primaryUrl;
 
   return productPrimaryPhotoUrl;
 };
 
 const getAllProductPhotos = async (id) => {
-  let productPhotos = await db.Photo.findOne({id: id}).select('primaryUrl productUrls');
+  let productPhotos = await Photo.findOne({ id: id }).select(
+    "primaryUrl productUrls"
+  );
   return productPhotos;
-}
+};
 
-const getMultipleProductsPrimaryPhotos = async(ids) => {
-  let primaryPhotosQuery = await db.Photo.find({id: {$in: ids}}).select('id primaryUrl');
+const getMultipleProductsPrimaryPhotos = async (ids) => {
+  let primaryPhotosQuery = await Photo.find({ id: { $in: ids } }).select(
+    "id primaryUrl"
+  );
   let primaryPhotos = {};
 
-  primaryPhotosQuery.forEach(photoInfo => {
-    primaryPhotos[photoInfo.id] = photoInfo.primaryUrl
+  primaryPhotosQuery.forEach((photoInfo) => {
+    primaryPhotos[photoInfo.id] = photoInfo.primaryUrl;
   });
   return new Promise((resolve, reject) => resolve(primaryPhotos));
-}
+};
 
 const getProductFeaturesPhotos = async (id) => {
-  let productInfo = await db.Photo.findOne({id: id}).select('id featuresUrls');
+  let productInfo = await Photo.findOne({ id: id }).select("id featuresUrls");
   return productInfo;
 };
 
@@ -32,5 +36,5 @@ module.exports = {
   getProductPrimaryPhoto: getProductPrimaryPhoto,
   getAllProductPhotos: getAllProductPhotos,
   getMultipleProductsPrimaryPhotos: getMultipleProductsPrimaryPhotos,
-  getProductFeaturesPhotos: getProductFeaturesPhotos
+  getProductFeaturesPhotos: getProductFeaturesPhotos,
 };
